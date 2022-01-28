8e11aea1c2477a43dba5156665af37e15f7b4b99
MrSumada <ricksamericana.com>
2022-01-28 13:34:25 -0500

    add appended recipe elements

--- js/recipes.js
+++ js/recipes.js
@@ -1,35 +1,12 @@
-var cuisine = "Italian"
-var recipeNumber = 0
+var cuisine = "American"
+var recipeNumber = Math.round(Math.random() * 20)
+console.log(recipeNumber);
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
     var options = {};
-    var instances = M.Carousel.init(elems, options);
+    // var instances = M.Carousel.init(elems, options);
 })
-
-// Spoontacular API
-
-// function recipeSearch () {
-//   fetch("https://api.spoonacular.com/food/products/search?query=" + cuisine + "&apiKey=df79ea3a515242fe939e0d371f9aeb53")
-//   .then(function(response) {
-//     return response.json();
-//   })
-//   .then(function(data) {
-//     console.log(data);
-//     recipeId = data.products[0].id;
-//     console.log(recipeId);
-
-//     fetch("https://api.spoonacular.com/recipes/" + recipeId + "/information&apiKey=df79ea3a515242fe939e0d371f9aeb53")
-//       .then(function(response){
-//         return response.json();
-//       })
-//       .then(function(data) {
-//         console.log(data);
-//       })
-//   })
-// }
-
-// recipeSearch();
 
 // theMealDb API
 
@@ -40,47 +17,31 @@ function mealSearch () {
   })
   .then(function(data) {
     console.log(data);
-    console.log(data.hits[0].recipe.label);
-    console.log(data.hits[0].recipe.images.REGULAR.url)
-    // $("#recipe-div").find("h1").text(data.hits[0].recipe.label);
-    // $("#recipe-div").append
+    console.log(data.hits[recipeNumber].recipe.label);
+    console.log(data.hits[recipeNumber].recipe.images.REGULAR.url);
+    var recipeImage = $("<img>").attr("src", data.hits[recipeNumber].recipe.images.REGULAR.url).addClass("responsive-img").attr("width", "100%").attr("height", "100%");
+    $(".recipe-image").append(recipeImage);
 
-  //   var recipeDiv = $("<div>", {id: "new-div", "class": "image-dive"});
-  //   var recipeImage = $("<img>", {"src": data.hits[0].recipe.images.REGULAR.url, "alt": "ADD ALT"});
-  //   $("#box").append(recipeDiv);
-  //   $("#new-div").append(recipeImage);
+    $(".recipe-content").find("h5").text(data.hits[recipeNumber].recipe.label);
 
-    // $("#recipe-div").css("background-image", "url(" + data.hits[0].recipe.images.REGULAR.url + ")");
-    
+    var source = $("<a>").text(data.hits[recipeNumber].recipe.source).attr("href", data.hits[recipeNumber].recipe.url);
+    $("#from").text("From: ")
+    $("#link").append(source);
+
+    var timeNum = data.hits[recipeNumber].recipe.totalTime;
+
+    if (timeNum !== 0) {
+      var time = $("<p>").text("Time: " + timeNum + " minutes");
+    } else {
+      var time = "Time: Unavailable"
+    }
+    $(".recipe-content").append(time);
+
+    var ingredients = data.hits[recipeNumber].recipe.ingredients;
+    for (var i = 0; i < ingredients.length; i++) {
+      $("<p>").text(ingredients[i].text).appendTo("#ingredients");
+    }
   })
 }
 
 mealSearch();
-
-// function recipeSearch () {
-//   fetch("https://api.spoonacular.com/food/products/search?query=" + cuisine + "&apiKey=df79ea3a515242fe939e0d371f9aeb53")
-//   // fetch("http://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata")
-//   // fetch("https://api.edamam.com/api/nutrition-details?app_id=b3dd42ee&app_key=722cf0bbfd82e53f97d6ac5ff393c653")
-//   // fetch("https://api.edamam.com/api/recipes/v2")
-//   // https:api.edamam.com/api/recipes/v2
-//   .then(function(response) {
-//     return response.json();
-//   })
-//   .then(function(data) {
-//     // recipeNumber = Math.random
-//     console.log(data);
-//     console.log(data.products[0]);
-//     $("#recipe-div").find("h1").text(data.products[0].title);
-//     // $("#recipe-div").css("background-image", "url(" + data.products[0].image + ")");
-//     recipeId = data.products[0].id;
-//     console.log(recipeId);
-
-//     fetch("https://api.spoonacular.com/recipes/" + recipeId + "/information&apiKey=df79ea3a515242fe939e0d371f9aeb53")
-//       .then(function(response){
-//         return response.json();
-//       })
-//       .then(function(data) {
-//         console.log(data);
-//       })
-//   })
-// }
